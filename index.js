999d98c097be691cd6cf6fa9d983588804582514
Nicolas HERMENT <nherment@arkhaios.net>
2017-05-10 10:50:25 +0200

    ability to inice css/sass

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
 
-
+const CleanCSS = require('clean-css')
+const sass = require('node-sass')
 const moment = require('moment')
+const logger = require('logacious')()
+const path = require('path')
+const fs = require('fs')
 
+let cssCleaner = new CleanCSS()
+let cssCache = {}
+let isProduction = process.env.NODE_ENV === 'production'
 
-module.exports = function(dust) {
+module.exports = function(dust, conf) {
+
+  if(!conf) {
+    logger.warn('portchain-dustjs-helpers: missing configuration')
+  }
+  
+  function loadCSS(src) {
+    let srcPath = path.resolve(path.join(conf.stylesDirectory, src))
+    let cssContent = fs.readFileSync(srcPath, 'utf-8')
+    if(/\.s[ac]ss$/.test(src)) {
+      cssContent = sass.renderSync({
+        data: cssContent,
+        includePaths: [conf.stylesDirectory]
+      }).css.toString()
+      console.log('SCSS', cssContent)
+    }
+    cssContent = cssCleaner.minify(cssContent).styles
+    return `<style>${cssContent}</style>`
+  }
+  
   dust.helpers.formatDate = (chunk, context, bodies, params) => {
     let value = context.resolve(params.date, chunk, context)
     let format = context.resolve(params.format, chunk, context)
     let output = moment(new Date(value)).format(format)
     return chunk.write(output)
   }
+  
   dust.helpers.formatBoolean = (chunk, context, bodies, params) => {
     let value = context.resolve(params.value, chunk, context)
     let output = value ? 'Yes' : 'No'
     return chunk.write(output)
+  }
+  
+  if(conf && conf.stylesDirectory) {
+    
+    dust.helpers.css = (chunk, context, bodies, params) => {
+      console.log('DUST HELPER', params.src)
+      let src = context.resolve(params.src, chunk, context)
+      let output = loadCSS(src)
+      return chunk.write(output)
+    }
+  } else {
+    logger.warn('portchain-dustjs-helpers: configuration is missing stylesDirectory field. Ignoring')
   }
 
   dust.helpers.iterate = (chunk, context, bodies) => {
